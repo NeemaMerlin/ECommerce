4d64a9fbcf3da39a5b6ab670a59ca4ae21e0ca54
NeemaMerlin <neemajaisonme@gmail.com>
2022-11-02 13:06:17 +0530

    herokucommit3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7w679cq0a&$mu4_^lyvlm_jsynsws7oeef8-%5megv^%ub31c9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
